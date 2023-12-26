0fdfbc3a6a948794bc872320761d4030c1b55f37
Yan Afriyoko <mail.yanafriyoko@gmail.com>
2023-12-26 15:35:27 +0700

    update endpoint backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,7 +154,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: 'http://127.0.0.1:8000/'
+    baseURL: 'https://nuxt.yanafriyoko.com/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
